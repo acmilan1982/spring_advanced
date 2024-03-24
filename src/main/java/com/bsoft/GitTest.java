50cb7d60c101d1ed85c11a3f49b2b87cb3391e8b
snoopy <17579163@qq.com>
2024-03-24 21:52:26 +0800

    hot-fix commit2

--- src/main/java/com/bsoft/GitTest.java
+++ src/main/java/com/bsoft/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello git!!!");
         System.out.println("33333333333333333333333");
-        System.out.println("hot fix");
+        System.out.println("hot fix test!");
     }
 }
