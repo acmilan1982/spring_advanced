237490365f67eb2c121b36d8ab0142e751cd81fb
snoopy <17579163@qq.com>
2024-03-24 21:29:57 +0800

    2nd commit

--- src/main/java/com/bsoft/GitTest.java
+++ src/main/java/com/bsoft/GitTest.java
@@ -3,5 +3,8 @@ package com.bsoft;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
     }
 }
